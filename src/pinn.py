d5f6cb9f86c1f30c424f1dcecfdcc718f0cc74c1
thash-jkr <afthashsahal123@gmail.com>
2025-04-06 08:52:35 +0930

    Commit to HPC

--- src/pinn.py
+++ src/pinn.py
@@ -21,15 +21,16 @@ m = me
 omega = 2 / hbar
 vQD = 15
 
-x_min = -75
-x_max = 150
-t_min = 0
-t_max = 20
-
 x0 = 0
 x1 = 75
 t0 = 0
 t1 = 2
+t2 = t1 + (x1 - x0) / vQD
+
+x_min = -75
+x_max = 150
+t_min = 0
+t_max = 20
 
 # device
 if torch.backends.mps.is_available():
@@ -44,14 +45,22 @@ else:
 
 # Define the PINN model
 class PINN(nn.Module):
-    def __init__(self, layers):
+    def __init__(self, layers, t_min, t_max):
         super(PINN, self).__init__()
         self.hidden_layers = nn.ModuleList()
+        
         for units in layers[1:-1]:
             self.hidden_layers.append(nn.Linear(in_features=layers[0], out_features=units))
             layers[0] = units
         
         self.output_layer = nn.Linear(layers[-2], layers[-1])
+
+        self.n_collocation = 3000
+        self.n_initial = 1000
+        self.n_boundary = 1000
+
+        self.t_min = t_min
+        self.t_max = t_max
     
     def forward(self, inputs):
         x, t = inputs
@@ -68,37 +77,98 @@ class PINN(nn.Module):
         
         return psi_real, psi_img
 
+    def generator(self, T_min, T_max):
+        t_collocation = np.random.uniform(T_min, T_max, self.n_collocation)
+        x_qd_collocation = np.where(t_collocation < t1, x0, np.where(t_collocation < t1 + (x1 - x0) / vQD, x0 + vQD * (t_collocation - t1), x1))
+        x_collocation = np.random.normal(loc=x_qd_collocation, scale=25.0, size=self.n_collocation)
+    
+        x_c = np.where(T_min < t1, x0, np.where(T_min < t1 + (x1 - x0) / vQD, x0 + vQD * (T_min - t1), x1))
+        x_initial = np.random.normal(loc=x_c, scale=25.0, size=self.n_initial)
+        t_initial = np.full(self.n_initial, T_min)
+        
+        x_boundary = np.concatenate([np.full(self.n_boundary // 2, x_min), np.full(self.n_boundary // 2, x_max)])
+        t_boundary = np.random.uniform(T_min, T_max, self.n_boundary)
+        
+        x_collocation_torch = torch.from_numpy(x_collocation).float().to(device)
+        t_collocation_torch = torch.from_numpy(t_collocation).float().to(device)
+        
+        x_initial_torch  = torch.from_numpy(x_initial).float().to(device)
+        t_initial_torch  = torch.from_numpy(t_initial).float().to(device)
+        
+        x_boundary_torch = torch.from_numpy(x_boundary).float().to(device)
+        t_boundary_torch = torch.from_numpy(t_boundary).float().to(device)
+    
+        return x_collocation_torch, t_collocation_torch, x_initial_torch, t_initial_torch, x_boundary_torch, t_boundary_torch
+
+    def loss_function(self, initial_condition, x_collocation_torch, t_collocation_torch, x_initial_torch, t_initial_torch, x_boundary_torch, t_boundary_torch):
+        #pde loss
+        x_collocation_torch = x_collocation_torch.clone().requires_grad_(True)
+        t_collocation_torch = t_collocation_torch.clone().requires_grad_(True)
+        
+        u, v = self((x_collocation_torch, t_collocation_torch))
+        
+        du_dt = torch.autograd.grad(u, t_collocation_torch, grad_outputs=torch.ones_like(u), create_graph=True)[0]
+        du_dx = torch.autograd.grad(u, x_collocation_torch, grad_outputs=torch.ones_like(u), create_graph=True)[0]
+        d2u_dx2 = torch.autograd.grad(du_dx, x_collocation_torch, grad_outputs=torch.ones_like(du_dx), create_graph=True)[0]
+        
+        dv_dt = torch.autograd.grad(v, t_collocation_torch, grad_outputs=torch.ones_like(v), create_graph=True)[0]
+        dv_dx = torch.autograd.grad(v, x_collocation_torch, grad_outputs=torch.ones_like(v), create_graph=True)[0]
+        d2v_dx2 = torch.autograd.grad(dv_dx, x_collocation_torch, grad_outputs=torch.ones_like(dv_dx), create_graph=True)[0]
+        
+        xqd_arr = torch.where(t_collocation_torch < t1, x0, torch.where(t_collocation_torch < t1 + (x1 - x0) / vQD, x0 + vQD * (t_collocation_torch - t1), x1))
+        
+        real = -hbar * dv_dt + ((hbar ** 2) / (2 * m)) * d2u_dx2 - 0.5 * m * (omega ** 2) * ((x_collocation_torch - xqd_arr) ** 2) * u
+        img = hbar * du_dt + ((hbar ** 2) / (2 * m)) * d2v_dx2 - 0.5 * m * (omega ** 2) * ((x_collocation_torch - xqd_arr) ** 2) * v
+        
+        physics_loss = torch.mean(real ** 2 + img ** 2)
+        
+        
+        
+        
+        #initial condition loss
+        u_i, v_i = self((x_initial_torch, t_initial_torch))
+        
+        psi_initial_real, psi_initial_img = initial_condition(x_initial_torch, t_initial_torch)
+        initial_condition_loss = torch.mean((u_i - psi_initial_real) ** 2) + torch.mean((v_i - psi_initial_img) ** 2)
+        
+        
+        
+        
+        #boundary condition loss
+        u_b, v_b = self((x_boundary_torch, t_boundary_torch))
+        boundary_condition_loss = torch.mean(u_b ** 2) + torch.mean(v_b ** 2)
+        
+        return physics_loss, initial_condition_loss, boundary_condition_loss
+
+    def train_model(self, optimizer, scheduler, initial_condition, epochs):
+        history = []
+        
+        for epoch in range(1, epochs+1):
+            optimizer.zero_grad()
+            
+            physics_loss, initial_condition_loss, boundary_condition_loss = self.loss_function(initial_condition, *self.generator(self.t_min, self.t_max))
+            total_loss = 10 * physics_loss + initial_condition_loss + boundary_condition_loss
+            
+            total_loss.backward()
+            optimizer.step()
+            scheduler.step()
+        
+            history.append({"total_loss": total_loss.item() ,"physics_loss": physics_loss.item(), "initial_condition_loss": initial_condition_loss.item(), "boundary_condition_loss": boundary_condition_loss.item()})
+            
+            if epoch % 1000 == 0:
+                print(f"Epoch {epoch}/{epochs}")
+                print(f"Total loss: {total_loss.item():.4e}")
+                print(f"Physics loss: {physics_loss.item():.4e}")
+                print(f"Initial condition loss: {initial_condition_loss.item():.4e}")
+                print(f"Boundary condition loss: {boundary_condition_loss.item():.4e}")
+                print("-" * 50)
+
+        return history
+
 layers = [2, 512, 512, 512, 512, 512, 512, 2]
 
-n_collocation = 3000
-n_initial = 1000
-n_boundary = 1000
-
-# Define dataset generator
-def generator():
-    t_collocation = np.random.uniform(t_min, t_max, n_collocation)
-    x_qd_collocation = np.where(t_collocation < t1, x0, np.where(t_collocation < t1 + (x1 - x0) / vQD, x0 + vQD * (t_collocation - t1), x1))
-    x_collocation = np.random.normal(loc=x_qd_collocation, scale=25.0, size=n_collocation)
-
-    x_initial = np.random.normal(loc=x0, scale=25.0, size=n_initial)
-    t_initial = np.full(n_initial, t_min)
-
-    x_boundary = np.concatenate([np.full(n_boundary // 2, x_min), np.full(n_boundary // 2, x_max)])
-    t_boundary = np.random.uniform(t_min, t_max, n_boundary)
-
-    x_collocation_torch = torch.from_numpy(x_collocation).float().to(device)
-    t_collocation_torch = torch.from_numpy(t_collocation).float().to(device)
-
-    x_initial_torch  = torch.from_numpy(x_initial).float().to(device)
-    t_initial_torch  = torch.from_numpy(t_initial).float().to(device)
-
-    x_boundary_torch = torch.from_numpy(x_boundary).float().to(device)
-    t_boundary_torch = torch.from_numpy(t_boundary).float().to(device)
-
-    return x_collocation_torch, t_collocation_torch, x_initial_torch, t_initial_torch, x_boundary_torch, t_boundary_torch
-
 # Model setup
-model = PINN(layers).to(device)
+model = PINN(layers, 0, t2).to(device)
 
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001, betas=(0.9, 0.9))
 
@@ -108,73 +178,12 @@ steps = 5000
 def exp_decay(step):
     return decay_rate ** (step / steps)
 
-# scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=exp_decay)
-scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=150000)
+scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=exp_decay)
 
-# Loss function
-def loss_function(x_collocation_torch, t_collocation_torch, x_initial_torch, t_initial_torch, x_boundary_torch, t_boundary_torch):
-    #pde loss
-    x_collocation_torch = x_collocation_torch.clone().requires_grad_(True)
-    t_collocation_torch = t_collocation_torch.clone().requires_grad_(True)
-    
-    u, v = model((x_collocation_torch, t_collocation_torch))
-    
-    du_dt = torch.autograd.grad(u, t_collocation_torch, grad_outputs=torch.ones_like(u), create_graph=True)[0]
-    du_dx = torch.autograd.grad(u, x_collocation_torch, grad_outputs=torch.ones_like(u), create_graph=True)[0]
-    d2u_dx2 = torch.autograd.grad(du_dx, x_collocation_torch, grad_outputs=torch.ones_like(du_dx), create_graph=True)[0]
-    
-    dv_dt = torch.autograd.grad(v, t_collocation_torch, grad_outputs=torch.ones_like(v), create_graph=True)[0]
-    dv_dx = torch.autograd.grad(v, x_collocation_torch, grad_outputs=torch.ones_like(v), create_graph=True)[0]
-    d2v_dx2 = torch.autograd.grad(dv_dx, x_collocation_torch, grad_outputs=torch.ones_like(dv_dx), create_graph=True)[0]
-    
-    xqd_arr = torch.where(t_collocation_torch < t1, x0, torch.where(t_collocation_torch < t1 + (x1 - x0) / vQD, x0 + vQD * (t_collocation_torch - t1), x1))
-    
-    real = -hbar * dv_dt + ((hbar ** 2) / (2 * m)) * d2u_dx2 - 0.5 * m * (omega ** 2) * ((x_collocation_torch - xqd_arr) ** 2) * u
-    img = hbar * du_dt + ((hbar ** 2) / (2 * m)) * d2v_dx2 - 0.5 * m * (omega ** 2) * ((x_collocation_torch - xqd_arr) ** 2) * v
-    
-    physics_loss = torch.mean(real ** 2 + img ** 2)
-    
-    
-    
-    
-    #initial condition loss
-    u_i, v_i = model((x_initial_torch, t_initial_torch))
-    
-    psi_initial_actual = (((m * omega) / (np.pi * hbar)) ** 0.25) * torch.exp(((-m * omega) / (2 * hbar)) * (x_initial_torch ** 2))
-    initial_condition_loss = torch.mean((u_i - psi_initial_actual) ** 2) + torch.mean((v_i - 0) ** 2)
-    
-    
-    
-    
-    #boundary condition loss
-    u_b, v_b = model((x_boundary_torch, t_boundary_torch))
-    boundary_condition_loss = torch.mean(u_b ** 2) + torch.mean(v_b ** 2)
-    
-    return physics_loss, initial_condition_loss, boundary_condition_loss
+def ground_state(x, t):
+    return (((m * omega) / (np.pi * hbar)) ** 0.25) * torch.exp(((-m * omega) / (2 * hbar)) * (x ** 2)), 0
 
-# Training loop
-epochs = 150000
-history = []
-
-for epoch in range(1, epochs+1):
-    optimizer.zero_grad()
-
-    physics_loss, initial_condition_loss, boundary_condition_loss = loss_function(*generator())
-    total_loss = 10 * physics_loss + initial_condition_loss + boundary_condition_loss
-
-    total_loss.backward()
-    optimizer.step()
-    scheduler.step()
-
-    history.append({"total_loss": total_loss.item(), "physics_loss": physics_loss.item(), "initial_condition_loss": initial_condition_loss.item(), "boundary_condition_loss": boundary_condition_loss.item()})
-
-    if epoch % 15000 == 0:
-        print(f"Epoch {epoch}/{epochs}")
-        print(f"Total loss: {total_loss.item():.4e}")
-        print(f"Physics loss: {physics_loss.item():.4e}")
-        print(f"Initial condition loss: {initial_condition_loss.item():.4e}")
-        print(f"Boundary condition loss: {boundary_condition_loss.item():.4e}")
-        print("-" * 50)
+history = model.train_model(optimizer, scheduler, ground_state, 150000)
 
 #saving
 import os
